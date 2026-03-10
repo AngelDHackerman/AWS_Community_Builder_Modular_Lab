24489603b9419d25e13e6afbad3155f613dae8bd
Angel Hernandez <angeldariaux@gmail.com>
2026-03-09 20:10:06 -0600

    serverless task api, agregada funcion handler con los metodos

--- cloud_engineer/01_serverless_task_api/src/handler.py
+++ cloud_engineer/01_serverless_task_api/src/handler.py
@@ -57,3 +57,61 @@ def require_authenticated_claims(event: dict) -> dict:
         # pero lo dejamos por seguridad defensiva.
         raise PermissionError("Missing or invalid JWT claims")
     return claims
+
+def audit_event(event_type: str, payload: dict):
+    ts = now_utc()
+    date_prefix = ts[0:10].replace("-", "/")
+    object_key = f"events/{date_prefix}/{event_type}_{uuid.uuid4()}.json"
+    
+    document = {
+        "event_type": event_type,
+        "timestamp": ts,
+        "payload": payload
+    }
+    
+    s3.put_object(
+        Bucket=AUDIT_BUCKET_NAME,
+        Key=object_key,
+        Body=json.dumps(document).encode("utf-8"),
+        ContentType="application/json"
+    )
+    
+def lambda_handler(event, context):
+    logger.info("Incoming event: %s", json.dumps(event))
+    
+    method = get_method(event)
+    task_id = get_path_param(event, "task_id")
+    
+    try:
+        if method == "GET" and task_id:
+            return get_task(task_id)
+        
+        if method == "GET":
+            return list_task()
+        
+        if method == "POST": 
+            return create_task(event)
+        
+        if method == "PATCH" and task_id:
+            return update_task(event, task_id)
+        
+        if method == "DELETE" and task_id:
+            return delete_task(event, task_id)
+        
+        return response(405, {"message": "Method not allowed"})
+    
+    except PermissionError as e:
+        logger.warning("Unauthorized request: %s", str(e))
+        return response(401, {"message": str(e)})
+    
+    except ClientError as e:
+        logger.exception("AWS client error")
+        return response(500, {"message": "Internal AWS error", "error": str(e)})
+    
+    except ValueError as e:
+        logger.warning("Bad request: %s", str(e))
+        return response(400, {"message": str(e)})
+    
+    except Exception as e:
+        logger.exception("Unhandled exception")
+        return response(500, {"message": "Unexpected internal error", "error": str(e)})
